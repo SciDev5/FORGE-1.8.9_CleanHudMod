7b191fbc9378b28060a23ade62882e6d975b26ec
SciDev5 <67175934+SciDev5@users.noreply.github.com>
2020-08-13 09:48:52 -0400

    Update version info

--- src/main/java/me/scidev/forge/cleanhudmod/HudMod.java
+++ src/main/java/me/scidev/forge/cleanhudmod/HudMod.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class HudMod
 {
 	public static final String MODID = "cleanhudmod";
-	public static final String VERSION = "1.1.1";
+	public static final String VERSION = "1.1.2";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	
 	@Instance
@@ -34,7 +34,7 @@ public class HudMod
 	public void init(FMLInitializationEvent event)
 	{
 		ConfigUtil.initConfig();
-		LOGGER.log(Level.INFO, "Init cleanhudmod v1.1.1");
+		LOGGER.log(Level.INFO, "Init cleanhudmod v1.1.2");
 		MinecraftForge.EVENT_BUS.register(new EventSubscriber());
 	}
 }
